c1807428289676a0e8bc8221181c09efd9e85018
ddorota <46536309+ddorota@users.noreply.github.com>
2019-02-09 17:12:31 +0100

    Add files via upload

---
+++ ddd.js
@@ -0,0 +1,15 @@
+function wypisz() {
+    var liczba1 = document.getElementById("pole1").value;
+    var liczba2 = document.getElementById("pole2").value;
+    var napis = " ";
+
+    for (i = liczba1; i <= liczba2; i++) {
+        console.log(i);
+        napis = napis + i + " ";
+    }
+    for (i = liczba1; i >= liczba2; i--) {
+        console.log(i);
+        napis = napis + i + " ";
+    }
+    document.getElementById("wynik2").innerHTML = napis;
+}
