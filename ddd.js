8724e3ed0f0fd9e6d991c2e7a6ac14d4446a3681
damianmarzec <damdam1990+github@gmail.com>
2019-02-09 17:44:51 +0100

    Update ddd.js
    Może tak

--- ddd.js
+++ ddd.js
@@ -1,13 +1,21 @@
-function wypisz() {
-    var liczba1 = document.getElementById("pole1").value;
-    var liczba2 = document.getElementById("pole2").value;
-    var napis = " ";
+function listAll(start, end) {
+  let napis = "";
+  for (i = start; i <= end; i++) {
+      console.log(i);
+      napis += i + " ";
+  }
+  return napis;
+}
 
-    for (i = liczba1; i <= liczba2; i++) {
-        napis = napis + i + " ";
-    }
-    for (i = liczba1; i >= liczba2; i--) {
-        napis = napis + i + " ";
+function wypisz() {
+  // parseInt (zmienia ciąg znaków na liczbę)
+    var liczba1 = parseInt(document.getElementById("pole1").value);
+    var liczba2 = parseInt(document.getElementById("pole2").value);
+    var napis;
+    if (liczba1 <= liczba2) {
+      napis = listAll(liczba1, liczba2);
+    } else {
+      napis = listAll(liczba2, liczba1);
     }
     document.getElementById("wynik2").innerHTML = napis;
 }
